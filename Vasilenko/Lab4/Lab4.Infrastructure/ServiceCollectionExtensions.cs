406f74351bb230ec98a51b3d02b1c691c5fd0fa2
Vasilenko Nataliya <nsvasilenko@gmail.com>
2018-11-29 16:36:28 +0300

    Lab4

---
+++ Vasilenko/Lab4/Lab4.Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Lab4.Data.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lab4.Infrastructure
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDbContext(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            serviceCollection.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            return serviceCollection;
+        }
+
+        public static IServiceCollection AddIdentity(this IServiceCollection serviceCollection)
+        {
+
+                serviceCollection.AddIdentity<ApplicationUser, IdentityRole>(options =>
+                {
+                    options.Password.RequiredLength = 6;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireDigit = false;
+                })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            return serviceCollection;
+        }
+    }
+}
